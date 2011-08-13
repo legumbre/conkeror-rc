ee12c4cc9af67b3c8c906b3d9277132325490a69
Leonardo Etcheverry <leo@kalio.net>
2011-08-12 21:26:59 -0300

    my gists webjump

--- my-webjumps.js
+++ my-webjumps.js
@@ -1,7 +1,7 @@
 // webjumps
 
-define_webjump("del"    , "http://delicious.com/search?p=%s&chk=&context=userposts%7C__USERNAME_HERE__&fr=del_icio_us&lc=1");
-// define_webjump("gist"   , "https://gist.github.com/mine");
+define_webjump("del"    , "http://delicious.com/search?p=%s&chk=&context=userposts%7C--USERNAME-HERE--&fr=del_icio_us&lc=1");
+define_webjump("mg"     , "https://gist.github.com/mine");
 define_webjump("hn"     , "http://news.ycombinator.com");
 define_webjump("klog"   , "https://www.kalio.net/cgi-bin/irclogger/irclogger_log/kalio");
 define_webjump("meebo"  , "https://meebo.com/messenger");
