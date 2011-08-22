230e768b47b32efc9e509147fbade31c5a312df1
Leonardo Etcheverry <leo@kalio.net>
2011-08-22 19:29:50 -0300

    smg == search my gists webjump

--- my-webjumps.js
+++ my-webjumps.js
@@ -1,10 +1,7 @@
 // webjumps
-
-define_webjump("del"    , "http://delicious.com/search?p=%s&chk=&context=userposts%7C--USERNAME-HERE--&fr=del_icio_us&lc=1");
-define_webjump("mg"     , "https://gist.github.com/mine");
 define_webjump("hn"     , "http://news.ycombinator.com");
-define_webjump("klog"   , "https://www.kalio.net/cgi-bin/irclogger/irclogger_log/kalio");
+define_webjump("mg"     , "https://gist.github.com/mine");
 define_webjump("meebo"  , "https://meebo.com/messenger");
 define_webjump("proggit", "http://reddit.com/r/programming");
-define_webjump("trac"   , "https://svn.kalio.net/trac/%s");
+define_webjump("smg"    , "http://www.google.com/search?q=site%3Agist.github.com+legumbre+%s");
 
