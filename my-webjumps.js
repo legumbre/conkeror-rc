dc9a2d297153979a5609294872bd20784d014c5c
Leonardo Etcheverry <leo@kalio.net>
2011-08-22 20:58:48 -0300

    mw, rae webjumps

--- my-webjumps.js
+++ my-webjumps.js
@@ -2,6 +2,8 @@
 define_webjump("hn"     , "http://news.ycombinator.com");
 define_webjump("mg"     , "https://gist.github.com/mine");
 define_webjump("meebo"  , "https://meebo.com/messenger");
+define_webjump("mw"     , "http://www.merriam-webster.com/dictionary/%s");
 define_webjump("proggit", "http://reddit.com/r/programming");
+define_webjump("rae"    , "http://rae.es/%s");
 define_webjump("smg"    , "http://www.google.com/search?q=site%3Agist.github.com+legumbre+%s");
 
