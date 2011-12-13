b6907fe1c9c9798e212667ffbeb24e1cb2b14fdf
Leonardo Etcheverry <leo@kalio.net>
2011-12-13 01:23:37 -0200

    removed proggit webjump

--- my-webjumps.js
+++ my-webjumps.js
@@ -1,10 +1,10 @@
 // webjumps
+define_webjump("pin"    , "http://pinboard.in/search/?query=%s+&mine=Search+Mine");
 define_webjump("hn"     , "http://news.ycombinator.com");
 define_webjump("hub"    , "http://github.com");
 define_webjump("mg"     , "https://gist.github.com/mine");
 define_webjump("meebo"  , "https://meebo.com/messenger");
 define_webjump("mw"     , "http://www.merriam-webster.com/dictionary/%s");
-define_webjump("proggit", "http://reddit.com/r/programming");
 define_webjump("rae"    , "http://rae.es/%s");
 define_webjump("smg"    , "http://www.google.com/search?q=site%3Agist.github.com+legumbre+%s");
 define_webjump("tpb"    , "https://thepiratebay.org/search/%s");
