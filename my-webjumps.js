4015317b089bcf32dd866d9f747c5cff37597354
Leonardo Etcheverry <leo@kalio.net>
2012-01-14 15:22:53 -0200

    twitter webjump

--- my-webjumps.js
+++ my-webjumps.js
@@ -1,12 +1,12 @@
 // webjumps
-define_webjump("pin"    , "http://pinboard.in/search/?query=%s+&mine=Search+Mine");
-define_webjump("hn"     , "http://news.ycombinator.com");
-define_webjump("hub"    , "http://github.com");
-define_webjump("mg"     , "https://gist.github.com/mine");
-define_webjump("meebo"  , "https://meebo.com/messenger");
-define_webjump("mw"     , "http://www.merriam-webster.com/dictionary/%s");
-define_webjump("rae"    , "http://rae.es/%s");
-define_webjump("read"   , "http://reader.google.com");
-define_webjump("smg"    , "http://www.google.com/search?q=site%3Agist.github.com+legumbre+%s");
-define_webjump("tpb"    , "https://thepiratebay.org/search/%s");
-
+define_webjump("pin"   , "http://pinboard.in/search/?query=%s+&mine=Search+Mine");
+define_webjump("hn"    , "http://news.ycombinator.com");
+define_webjump("hub"   , "http://github.com");
+define_webjump("mg"    , "https://gist.github.com/mine");
+define_webjump("meebo" , "https://meebo.com/messenger");
+define_webjump("mw"    , "http://www.merriam-webster.com/dictionary/%s");
+define_webjump("rae"   , "http://rae.es/%s");
+define_webjump("read"  , "http://reader.google.com");
+define_webjump("smg"   , "http://www.google.com/search?q=site%3Agist.github.com+legumbre+%s");
+define_webjump("tpb"   , "https://thepiratebay.org/search/%s");
+define_webjump("tw"    , "https://twitter.com/home");
