dea7cb8e7d7ebe34c6f760f50573eda5ed74e2dc
Leonardo Etcheverry <leo@kalio.net>
2012-04-26 19:48:29 -0300

    show loading_count_widget

--- conkerorrc.js
+++ conkerorrc.js
@@ -45,6 +45,9 @@ read_buffer_show_icons = true;
 // show buffer count in modeline
 add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
 
+// show loading buffer count widget
+add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
+
 // conkeror on mac X11 meta workaround
 define_key(default_global_keymap, "x", "execute-extended-command");
 define_key(content_buffer_normal_keymap, "x", "execute-extended-command");
