d41ca7734e5bb1704e231b062aad24e368608306
Leonardo Etcheverry <leo@kalio.net>
2012-05-08 00:46:37 -0300

    .dir-locals.el with conkeror dev guidelines

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((js-mode
-  (js-indent-level . 2)))
+  (js-indent-level . 4)
+  (whitespace-style . (face lines))
+  (whitespace-line-column . 80)
+  (mode . whitespace)))
+
