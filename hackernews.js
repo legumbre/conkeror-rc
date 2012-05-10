64bfcfb01cc5e75698ad12934d6006ed732df3c2
Leonardo Etcheverry <leo@kalio.net>
2012-05-09 22:59:38 -0300

    hackernews: end of page behavior var

--- hackernews.js
+++ hackernews.js
@@ -21,6 +21,14 @@
 
 require("content-buffer.js");
 
+define_variable("hackernews_end_behavior", "page", 
+    "Controls the behavior of hackernews-next-post and "+
+    "hackernews-prev-post when at the last post of the page.  "+
+    "Possible values are: 'stop', 'wrap' and 'page'."+
+    "  'stop' means not change the selected post."+
+    "  'wrap' means to wrap around to the first post in the page."+
+    "  'page' means to advance to the next page. ");
+
 /*
  * User stylesheet
  * Change highlight colors here.
@@ -39,25 +47,81 @@ register_user_stylesheet(
 ));
 
 
-/*
- * Select and return the next element among filtered nodes in a
- * circular fashion, according to `direction'. Selecting a post means
- * adding the class maker_class to the selected element.
+/* Find the next item using the `dom_filter' function and select it by
+ * adding the class `marker_class' to it.  When the last item is
+ * reached, the behavior is controlled by the hackernews_end_behavior
+ * variable. 
  */
-function hackernews_next_item (I, dom_filter, direction, marker_class) {
-    var nodes = dom_filter(I);
-    var cp = nodes.filter(function (p) { return (p.className.indexOf(marker_class) >= 0); });
+function hackernews_next_item (I, dom_filter, marker_class) {
+    var doc = I.buffer.document;
+    var items = dom_filter(I);
+    var sel_items = items.filter(function (p) { return (p.className.indexOf(marker_class) >= 0); });
+    var current   = sel_items.length ? sel_items[0] : null;
+    var current_i = current ? items.indexOf(current) : -1;
 
-    var current = (cp.length != 0) ? cp[0] : nodes[nodes.length-1] ;
-    var nexti = (nodes.indexOf(current) + direction) % nodes.length
-    var next = nodes[nexti]
+    var next_i    = current_i + 1;
+    var next_page = null;
 
+    switch (hackernews_end_behavior) {
+    case "stop":
+        next_i = (next_i < items.length) ? next_i : (items.length - 1);
+        break;
+    case "wrap":
+        next_i = next_i % items.length;
+        break;
+    case "page":
+        if (next_i == items.length) {
+            next_page = doc.querySelector("a[rel^=next]");
+        }
+        break;
+    }
+
+    // advance to the next page if necessary
+    if (next_page) {
+        browser_object_follow(I.buffer, FOLLOW_DEFAULT, next_page);
+        return next_page;
+    }
+
+    // select the next item by adding marker_class 
+    var next = items[next_i]
     if (current)
         dom_remove_class(current, marker_class);
     dom_add_class(next, marker_class);
 
     return next;
 }
+
+/* Find the previous item using the `dom_filter' function and select
+ * it by adding the class `marker_class' to it.  
+ */
+function hackernews_prev_item (I, dom_filter, marker_class) {
+    var items = dom_filter(I);
+    var sel_items = items.filter(function (p) { return (p.className.indexOf(marker_class) >= 0); });
+    var current   = sel_items.length ? sel_items[0] : null;
+    var current_i = current ? items.indexOf(sel_items[0]) : items.length;
+
+    var prev_i = current_i - 1;
+
+    switch (hackernews_end_behavior) {
+    case "stop":
+        prev_i = (prev_i >= 0) ? prev_i : 0;
+        break;
+    case "wrap":
+        prev_i = (prev_i % items.length + items.length) % items.length;
+        break;
+    case "page":
+        // same as 'stop' since there's no prev page in HN
+        // TODO: fix this if HN ever publishes such a link
+        prev_i = (prev_i >= 0) ? prev_i : 0; 
+        break;
+    }
+    var prev = items[prev_i];
+    if (current)
+        dom_remove_class(current, marker_class);
+    dom_add_class(prev, marker_class);
+    return prev;
+}
+
 
 function hackernews_focus_post (I, post) {
     hackernews_focus(I, post, ".title a");
@@ -122,15 +186,15 @@ function _hackernews_comment_filter (I) {
 interactive("hackernews-next-post",
     "Select the next post.",
     function (I) {
-        var next_post = hackernews_next_item(I, _hackernews_post_filter, 1 /* down */, "current");
-        hackernews_focus_post(I, next_post);
+        var next_post = hackernews_next_item(I, _hackernews_post_filter, "current");
+        if (next_post) hackernews_focus_post(I, next_post);
     });
 
 interactive("hackernews-prev-post",
     "Select the previous post.",
     function (I) {
-        var prev_post = hackernews_next_item(I, _hackernews_post_filter, -1 /* up */, "current");
-        hackernews_focus_post(I, prev_post);
+        var prev_post = hackernews_prev_item(I, _hackernews_post_filter, "current");
+        if (prev_post) hackernews_focus_post(I, prev_post);
     });
 
 interactive("hackernews-vote-up-post",
@@ -216,15 +280,15 @@ page_mode_activate(hackernews_mode);
 interactive("hackernews-next-comment",
     "Focus next Hacker News comment.",
     function (I) {
-        var next_comment = hackernews_next_item(I, _hackernews_comment_filter, 1 /* down */, "current-comment");
-        hackernews_focus_comment(I, next_comment);
+        var next_comment = hackernews_next_item(I, _hackernews_comment_filter, "current-comment");
+        if (next_comment) hackernews_focus_comment(I, next_comment);
     });
 
 interactive("hackernews-prev-comment",
     "Focus previous Hacker News comment",
     function (I) {
-        var prev_comment = hackernews_next_item(I, _hackernews_comment_filter, -1 /* down */, "current-comment");
-        hackernews_focus_comment(I, prev_comment);
+        var prev_comment = hackernews_prev_item(I, _hackernews_comment_filter, "current-comment");
+        if (prev_comment) hackernews_focus_comment(I, prev_comment);
     });
 
 interactive("hackernews-vote-up-comment",
