8d3240d1352a640cf9af1a5a7ee1471b05a4dcb6
Leonardo Etcheverry <leo@kalio.net>
2012-05-18 15:04:34 -0300

    add stylesheets.js

---
+++ stylesheets.js
@@ -0,0 +1,16 @@
+/*
+ * My custom stylesheets.
+ */
+
+
+/* mobile twitter*/
+register_user_stylesheet(
+    "data:text/css," +
+        escape (
+            "@-moz-document url-prefix(https://mobile.twitter.com/) {" +
+                "#container {" +
+                "   width: 98% !important;" +
+                "}" +
+            "}"
+));
+
